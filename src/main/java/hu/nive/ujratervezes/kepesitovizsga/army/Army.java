7892149ad9b8e66cc52350b1d1e8782304493a33
Anett <holcza@gmail.com>
2021-04-29 11:45:29 +0200

    Army feladat

--- src/main/java/hu/nive/ujratervezes/kepesitovizsga/army/Army.java
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/army/Army.java
@@ -1,9 +1,36 @@
 package hu.nive.ujratervezes.kepesitovizsga.army;
 
-public abstract class Army {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Army {
 
-    int hitpoints;
+    private List<MilitaryUnit> militaryUnits = new ArrayList<>();
 
+    public void addUnit(MilitaryUnit militaryUnit) {
+        militaryUnits.add(militaryUnit);
+    }
 
+    public void damageAll(int damage) {
+        List<MilitaryUnit> removeMilitaryUnits = new ArrayList<>();
+        for (MilitaryUnit mu : militaryUnits) {
+            mu.sufferDamage(damage);
+            if (mu.getHitPoints() < 25) {
+                removeMilitaryUnits.add(mu);
+            }
+        }
+        militaryUnits.removeAll(removeMilitaryUnits);
+    }
+
+    public int getArmyDamage() {
+        int sum = 0;
+        for (MilitaryUnit mu : militaryUnits) {
+            sum += mu.doDamage();
+        }
+        return sum;
+    }
+
+    public int getArmySize() {
+        return militaryUnits.size();
+    }
 }
